a1c29c420515ebd48fd6543b2bb3ffc532000c15
ImLecus <marcosgf2005@gmail.com>
2022-11-23 18:14:28 +0100

    Using react

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Window from './components/window'
+import './index.css'
+
+var form = (
+  <form>
+    Initial blob number: <input type="number" min="0" max="255"/> <br />
+    Food number: <input type="number" min="0" max="511"/>  <br />
+    Generation time: <input type="number" min="0" max="511" defaultValue="15"/> seconds <br />
+    <input type="submit" value="Start simulation" />
+  </form>
+)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Window name="Options" content={form} />
+    <Window name="Game" content={""} />
+    <Window name="Statistics" content={ <>Generations : 1</>} />
+  </React.StrictMode>
+)
