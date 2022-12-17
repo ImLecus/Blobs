dfe6c1b7f3fb37ef7308149249d85d29b11d7bf0
ImLecus <marcosgf2005@gmail.com>
2022-12-17 18:18:32 +0100

    food created

--- src/components/simulation.jsx
+++ src/components/simulation.jsx
@@ -1,10 +1,35 @@
-import {Canvas} from 'react-three-fiber'
+import {Canvas} from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import { randInt } from 'three/src/math/MathUtils';
 
-function Box(){
+function Blob(){
     return(
-        <mesh>
+        <mesh position={[0,1,0]} onUpdate={blobBehaviour}>
             <boxGeometry attach="geometry"/>
-            <meshBasicMaterial attach="material" color="blue"/>
+            <meshStandardMaterial attach="material" color="green"/>
+        </mesh>
+    );
+}
+
+function blobBehaviour(){
+    console.log("hey")
+    
+}
+
+function Plane(){
+    return(
+        <mesh rotation={[-Math.PI/2,0,0]}>
+            <planeGeometry attach="geometry" args={[40,40]}/>
+            <meshStandardMaterial attach="material" color="white"/>
+        </mesh>
+    );
+}
+
+function Food(){
+    return(
+        <mesh position={[randInt(-15,15),0.5,randInt(-15,15)]} scale={0.3}>
+            <sphereGeometry attach="geometry"/>
+            <meshStandardMaterial attach="material" color="yellow"/>
         </mesh>
     );
 }
@@ -13,7 +38,21 @@ export default function Sim(){
     return(
         <div id="simulation">
             <Canvas>
-                <Box></Box>
+                <OrbitControls maxPolarAngle={Math.PI/2 - 0.25}/>
+                <Plane />
+                <Blob />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <Food />
+                <ambientLight intensity={0.5}/>
+                <spotLight position={[20,20,20]} intensity={0.5}/>
             </Canvas>
         </div>
     );
